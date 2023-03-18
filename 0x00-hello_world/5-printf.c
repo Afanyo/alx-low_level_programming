1a042c8eddb3e229839b2f5258dfd95dd95bec72
Afanyo <afanyotay@gmail.com>
2023-03-18 04:18:31 -0700

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,11 @@
 /**
  * main - print a c progam using printf
  *
- * Return: 0
+ * return: 0
 */
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
