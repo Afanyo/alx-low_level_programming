779782a1749e8626f32d5a23487add7a36b3849e
Afanyo <afanyotay@gmail.com>
2023-03-21 16:27:05 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+*main - function prints text as output
+*
+*Return:return 0
+*/
+
+int main(void)
+{
+char ch;
+for (ch = '_putchar' ; ch <= 'f' ; ch++)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
+}
